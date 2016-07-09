8c8d406a0eeef79437a675a6d1c827017ca76856
vinayvalageri <vinay.1si13cs131@gmail.com>
2016-07-09 21:05:10 +0530

    camera files1

--- SurveyActivity.java
+++ SurveyActivity.java
@@ -2,7 +2,9 @@ package com.example.vinay.sqllogin;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +21,9 @@ public class SurveyActivity extends AppCompatActivity {
     EditText e1,e2,e3,e4,e5;
     ImageView iv;
     Button sub,p;
+    protected Bitmap yourbitmap;
     private static final int CAMERA_PIC_REQUEST = 1111;
+    private static final int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,15 @@ public class SurveyActivity extends AppCompatActivity {
         e4=(EditText)findViewById(R.id.occupation);
         e5=(EditText)findViewById(R.id.phone);
         iv=(ImageView)findViewById(R.id.imageView);
+        iv.setImageResource(R.drawable.no_user);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent =new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);//load only one image*/
+                // selectImage();
+            }
+        });
     }
     public void photo(View view) {
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
@@ -39,26 +52,17 @@ public class SurveyActivity extends AppCompatActivity {
 
     }
 
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_PIC_REQUEST) {
-            //2
-            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-            iv.setImageBitmap(thumbnail);
-            //3
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-            //4
-            File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
-            try {
-                file.createNewFile();
-                FileOutputStream fo = new FileOutputStream(file);
-                //5
-                fo.write(bytes.toByteArray());
-                fo.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RESULT_LOAD_IMAGE && resultCode==RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            iv.setImageURI(selectedImage);
+
+            iv.buildDrawingCache();
+            yourbitmap = iv.getDrawingCache();
+
         }
+
     }
 }
