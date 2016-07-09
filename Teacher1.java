96842d998c94d6fe908a573f147bd2f7d4774be3
S.M.K.Murthy <smkmurthy@outlook.com>
2016-07-09 20:17:30 +0530

    anther commit

---
+++ Teacher1.java
@@ -0,0 +1,76 @@
+package com.webtechproject.webtech1.cfg1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Teacher1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher1);
+        Button myButton = (Button)findViewById(R.id.button1);
+        myButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Teacher1.this, uploadprogress1.class));
+            }
+
+        });
+
+        Button myButton1 = (Button)findViewById(R.id.button2);
+        myButton1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Teacher1.this, studentregistration.class));
+            }
+
+        });
+
+        /*
+        public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(Bundle savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    Button myButton = findViewById(R.id.new_button);
+    myButton.setOnClickListener(new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this, InsurancePage.class));
+        }
+
+    }
+         */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_teacher1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ mysms.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
+<?php
+$post_data = array(
+    'From'   => '8808891988',
+    'To'    => '9951215997',
+    'Body'  => 'hii', 
+);
+ 
+$exotel_sid = "jpmc51"; // Your Exotel SID - Get it from here: http://my.exotel.in/Exotel/settings/site#api-settings
+$exotel_token = "02188acb7c5c6251fab8c4c452f5c584e24e368d"; // Your exotel token - Get it from here: http://my.exotel.in/Exotel/settings/site#api-settings
+ 
+$url = "https://".$exotel_sid.":".$exotel_token."@twilix.exotel.in/v1/Accounts/".$exotel_sid."/Sms/send";
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+ 
+$http_result = curl_exec($ch);
+$error = curl_error($ch);
+$http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
+ 
+curl_close($ch);
+ 
+print "Response = ".print_r($http_result);
+?>
+</body>
+</html>
