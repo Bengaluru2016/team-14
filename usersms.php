ff42ed99b7841db14ca920c91fa4ec951d1a288c
S.M.K.Murthy <smkmurthy@outlook.com>
2016-07-10 06:14:10 +0530

    smsfunction

--- usersms.php
+++ usersms.php
@@ -1,38 +1,31 @@
-<html>
-<head>
-<title>Online PHP Script Execution</title>
-</head>
-<body>
-	<br>hello world <br><br>
-<?php
-$post_data = array(
-    'From' =>  '7382694782',
-    'To'    => '9160791337',
-    'Body'  => 'Thank You for Regitering in Samridhi NGO. Your username is_________ and password is ______', 
 
-);
+
+<?php 
+	function userregsms($mobile='1234567890',$body='abcd'){
+
+		$post_data = array(
+    
+    'To'    => '.$mobile.',
+    'Body'  => $body;
+
+		);
+
+		$exotel_sid = "jpmc51"; // Your Exotel SID - Get it from here: http://my.exotel.in/Exotel/settings/site#api-settings
+		$exotel_token = "02188acb7c5c6251fab8c4c452f5c584e24e368d"; // Your exotel token - Get it from here: http://my.exotel.in/Exotel/settings/site#api-settings
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+		$http_result = curl_exec($ch);
+		$error = curl_error($ch);
+	$http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
  
-$exotel_sid = "jpmc51"; // Your Exotel SID - Get it from here: http://my.exotel.in/Exotel/settings/site#api-settings
-$exotel_token = "02188acb7c5c6251fab8c4c452f5c584e24e368d"; // Your exotel token - Get it from here: http://my.exotel.in/Exotel/settings/site#api-settings
- 
-$url = "https://".$exotel_sid.":".$exotel_token."@twilix.exotel.in/v1/Accounts/".$exotel_sid."/Sms/send";
- 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FAILONERROR, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
- //echo $http_result;
-$http_result = curl_exec($ch);
-$error = curl_error($ch);
-$http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
- 
-curl_close($ch);
- 
-print "Response = ".print_r($http_result);
+	curl_close($ch);
+	print "Response = ".print_r($http_result);
+	}
+
 ?>
-</body>
-</html>
