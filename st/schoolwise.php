a61f85ab2f9b71e918f92443a9d36e17390eacf3
vivacse <vivacse@gmail.com>
2016-07-10 07:59:00 +0530

    mobileweb

---
+++ st/schoolwise.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang = "en">
+   
+   <head>
+      <meta charset = "utf-8">
+      <meta http-equiv = "X-UA-Compatible" content = "IE = edge">
+      <meta name = "viewport" content = "width = device-width, initial-scale = 1">
+      <title>Samridhdhi Trust</title>
+      <link href="signin.css" rel="stylesheet">
+	  <link href="css/bootstrap.min.css" rel="stylesheet">
+     
+   </head>
+   
+   <body>
+     
+	  <div class="container">
+		
+     <form class="signin"  action="login.php" method=POST>
+        <h2 class="form-signin-heading">Sign in</h2>
+        <label for="text" class="sr-only">Username</label>
+        <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
+       
+          
+		  
+        
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Log-in</button>
+		<label>
+			<p> Contact technical team for Login issues.</p>
+        </label>     
+	 </form>
+	  
+		
+    </div> 
+      
+    
+   </body>
+</html>
